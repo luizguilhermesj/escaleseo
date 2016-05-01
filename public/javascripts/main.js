182c42f1ef7f412196aa21d826cce27c5ce4a9c4
Luiz Guilherme <luizguilherme@recebedor.com>
2016-05-01 15:50:19 -0300

    Sharpening layout

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
 
         var rendered ="";
         for (var i=0; i<data.length; i++) {
+            data[i].language = data[i].language.toLowerCase().replace(/\+/g,'plus');
+            data[i].language = data[i].language.replace(/\#/g,'sharp');
+            data[i].language = (data[i].language == 'html') ? 'html5' : data[i].language;
             rendered += Mustache.render(template, data[i]);
         }
         $('#starred-repos').html(rendered);
